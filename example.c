0a677d75dab4775cf6d13d8f0f1c8082436ba3d0
Blain Smith <rebelgeek@blainsmith.com>
2019-06-18 17:53:20 -0400

    update the example and readme

--- example.c
+++ example.c
@@ -18,9 +18,9 @@ int main(int argc, char **argv) {
     seahash_final(digest, &context);
 
     for (uint8_t i = 0; i < SEAHASH_DIGEST_LENGTH; i++) {
-        printf("0x%02x ", digest[i]);
+        printf("0x%02x, ", digest[i]);
     }
-    // Prints "0x1b 0x99 0x3a 0x82 0x6f 0x4a 0xe5 0x75"
+    // Prints "0x79, 0xb2, 0x6b, 0x5a, 0x61, 0x6e, 0x2d, 0x4a"
 
     // This is a convenience function
     // for perorming the same hashing
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     seahash(hash, (const unsigned char *)str, strlen(str));
 
     for (uint8_t i = 0; i < SEAHASH_DIGEST_LENGTH; i++) {
-        printf("0x%02x ", hash[i]);
+        printf("0x%02x, ", hash[i]);
     }
-    // Prints "0x1b 0x99 0x3a 0x82 0x6f 0x4a 0xe5 0x75"
+    // Prints "0x79, 0xb2, 0x6b, 0x5a, 0x61, 0x6e, 0x2d, 0x4a"
 }
